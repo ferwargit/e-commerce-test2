502469aeccfd348bc8a55dacb298db7472baa1eb
Fernando Warno <ferwar@hotmail.com>
2025-07-05 16:50:13 -0300

    Agregar componentes Header, Main, Nav y Footer para estructurar la aplicación

---
+++ e-commerce-1.0/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#f1f1f1",
+        padding: "10px",
+        textAlign: "center",
+        marginTop: "20px",
+        color: "#333",
+      }}
+    >
+      <p>&copy; 2025 - Mi Aplicación React</p>
+    </footer>
+  );
+}
+
+export default Footer;
