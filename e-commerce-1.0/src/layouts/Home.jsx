79bcbb6281dd57b8e09c727ab1f4cd439bfea104
Fernando Warno <ferwar@hotmail.com>
2025-07-05 16:57:08 -0300

    Reemplazar el título por componentes de navegación y contenido en el layout Home

--- e-commerce-1.0/src/layouts/Home.jsx
+++ e-commerce-1.0/src/layouts/Home.jsx
@@ -1,7 +1,15 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Nav from "../components/Nav";
+
 function Home() {
   return (
     <div>
-      <h1>HOME</h1>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
