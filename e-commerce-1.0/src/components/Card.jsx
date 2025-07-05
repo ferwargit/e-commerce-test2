e7f7102307051f04ec60a5aacaea4f3f8b731d3b
Fernando Warno <ferwar@hotmail.com>
2025-07-05 20:32:16 -0300

    Agregar estilos a los componentes de Card y actualizar el layout de Home para incluir ProductosContainer

--- e-commerce-1.0/src/components/Card.jsx
+++ e-commerce-1.0/src/components/Card.jsx
@@ -3,13 +3,15 @@ import "../styles/Productos.css";
 function Card({ producto }) {
   return (
     <div className="producto-card">
-      <h3>{producto.nombre}</h3>
-      <p>{producto.descripcion}</p>
+      <h3 style={{color:"black"}}>{producto.nombre}</h3>
+      <p style={{color:"black"}}>{producto.descripcion}</p>
       <img
         className="producto-image"
         src={producto.imagen}
         alt={producto.nombre}
       />
+      <p style={{color:"black"}}>Precio: ${producto.precio}</p>
+      <button style={{color:"black"}} className="producto-button">Agregar al carrito</button>
     </div>
   );
 }
--- e-commerce-1.0/src/layouts/Home.jsx
+++ e-commerce-1.0/src/layouts/Home.jsx
@@ -2,14 +2,55 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Nav from "../components/Nav";
+import ProductosContainer from "../components/ProductosContainer";
 
 function Home() {
+  const productos = [
+    {
+      id: 1,
+      nombre: "Producto 1",
+      descripcion: "Descripción del Producto 1",
+      precio: 100,
+      imagen:
+        "https://cdn.pixabay.com/photo/2021/01/06/07/46/lipstick-5893456_1280.jpg",
+    },
+    {
+      id: 2,
+      nombre: "Producto 2",
+      descripcion: "Descripción del Producto 2",
+      precio: 200,
+      imagen:
+        "https://cdn.pixabay.com/photo/2021/08/03/06/47/perfume-6518634_1280.jpg",
+    },
+    {
+      id: 3,
+      nombre: "Producto 3",
+      descripcion: "Descripción del Producto 3",
+      precio: 300,
+      imagen:
+        "https://cdn.pixabay.com/photo/2017/03/02/02/46/glasses-2110274_1280.jpg",
+    },
+    {
+      id: 4,
+      nombre: "Producto 4",
+      descripcion: "Descripción del Producto 4",
+      precio: 400,
+      imagen:
+        "https://cdn.pixabay.com/photo/2017/07/01/20/51/mug-2462665_1280.png",
+    },
+  ];
   return (
     <div>
-      <Nav />
-      <Header />
-      <Main />
-      <Footer />
+      <div>
+        <Nav />
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+      <div>
+        <Nav />
+        <ProductosContainer productos={productos} />
+      </div>
     </div>
   );
 }
