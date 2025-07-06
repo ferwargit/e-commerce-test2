3ccb5ea88274d86024607b27903d7ce2fae858ed
Fernando Warno <ferwar@hotmail.com>
2025-07-06 15:54:22 -0300

    Eliminamos ProductosContainer y Footer de Home.jsx

--- e-commerce-1.0/src/layouts/Home.jsx
+++ e-commerce-1.0/src/layouts/Home.jsx
@@ -1,20 +1,13 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
-import ProductosContainer from "../components/ProductosContainer";
 
 function Home() {
   return (
     <div>
-      <div>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-      <div>
-        <ProductosContainer />
-        <Footer />
-      </div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
