c6e1c353f1cdea732dc8fc4c759fb91e6b81d424
Fernando Warno <ferwar@hotmail.com>
2025-07-06 15:27:21 -0300

    Ajustamos App con React-Router

--- e-commerce-1.0/src/App.jsx
+++ e-commerce-1.0/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import Home from "./layouts/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productos" element={<p>Productos</p>} />
+          <Route path="/carrito" element={<p>Carrito</p>} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
