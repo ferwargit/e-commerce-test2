52bb6e1d93b182611dec6ce81e1aba938027aa63
Fernando Warno <ferwar@hotmail.com>
2025-07-06 15:46:01 -0300

    Añadimos Nav en App.jsx

--- e-commerce-1.0/src/App.jsx
+++ e-commerce-1.0/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
+import Nav from "./components/Nav";
 import Home from "./layouts/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <>
+      <div>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/productos" element={<p>Productos</p>} />
           <Route path="/carrito" element={<p>Carrito</p>} />
         </Routes>
-      </>
+      </div>
     </Router>
   );
 }
