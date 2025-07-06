11be783b992fe0968d7876b06bcc7b48e13e1621
Fernando Warno <ferwar@hotmail.com>
2025-07-06 01:57:11 -0300

    Refactor ProductosContainer: eliminar propiedad de productos y simplificar manejo de estado de carga y error.

--- e-commerce-1.0/src/components/ProductosContainer.jsx
+++ e-commerce-1.0/src/components/ProductosContainer.jsx
@@ -4,26 +4,32 @@ import "../styles/Productos.css";
 import "../styles/Carrito.css";
 import Carrito from "./Carrito";
 
-function ProductosContainer({ productos }) {
+function ProductosContainer() {
+  const [productos, setProductos] = useState([]);
   const [productosCarrito, setProductosCarito] = useState([]);
   const [cargando, setCargando] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    // fetch("https://680f75cd67c5abddd195693e.mockapi.io/products")
-    fetch("https://6869ee8c2af1d945cea2cfff.mockapi.io/productos")
-      .then((respuesta) => respuesta.json())
-      .then((datos) => {
-        console.log("Datos obtenidos:", datos);
-        setCargando(false);
-      })
-      .catch((error) => {
-        console.error("Error al obtener los datos:", error);
-        setError("Hubo un problema al cargar los productos");
-        setCargando(false);
-      });
-  }, []); // El array vacío asegura que el efecto se ejecute solo una vez al montar el componente
+  {
+    useEffect(() => {
+      fetch("https://6869ee8c2af1d945cea2cfff.mockapi.io/productos")
+        .then((respuesta) => {
+          return respuesta.json();
+        })
+        .then((datos) => {
+          console.log("Datos obtenidos:", datos);
+          setProductos(datos);
+          setCargando(false);
+        })
+        .catch((error) => {
+          console.error("Error al obtener los datos:", error);
+          setError("Hubo un problema al cargar los productos");
+          setCargando(false);
+        });
+    }, []);
+  }
 
+  // Función para agregar un producto al carrito
   function funcionCarrito(producto) {
     console.log(`Paso 1 - Agregando producto al carrito: ${producto.nombre}`);
     console.log(
@@ -43,22 +49,22 @@ function ProductosContainer({ productos }) {
     return <p>Error: {error}</p>;
   } else {
     return (
-      <>
-        <div>
-          <div className="productos-container">
-            {productos.map((producto) => (
-              <Card
-                key={producto.id}
-                producto={producto}
-                funcionCarrito={funcionCarrito}
-              />
-            ))}
-          </div>
+      <div>
+        <div className="productos-container">
+          {productos.map((producto) => (
+            <Card
+              key={producto.id}
+              producto={producto}
+              funcionCarrito={funcionCarrito}
+            />
+          ))}
+        </div>
+        {
           <div>
             <Carrito productosCarrito={productosCarrito} />
           </div>
-        </div>
-      </>
+        }
+      </div>
     );
   }
 }
