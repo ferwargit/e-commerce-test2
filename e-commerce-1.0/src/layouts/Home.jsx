5e2d7c44c6e67d29fab18374c82fdb908222b26b
Fernando Warno <ferwar@hotmail.com>
2025-07-06 15:49:09 -0300

    Ajustamos Home.jsx para navegar por las rutas

--- e-commerce-1.0/src/layouts/Home.jsx
+++ e-commerce-1.0/src/layouts/Home.jsx
@@ -1,20 +1,17 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
-import Nav from "../components/Nav";
 import ProductosContainer from "../components/ProductosContainer";
 
 function Home() {
   return (
     <div>
       <div>
-        <Nav />
         <Header />
         <Main />
         <Footer />
       </div>
       <div>
-        <Nav />
         <ProductosContainer />
         <Footer />
       </div>
