6ef4ae0f87d96930defd3038cbff45c92e74a7c1
Fernando Warno <ferwar@hotmail.com>
2025-07-07 15:23:05 -0300

    Añade Link a la imagen del producto

--- e-commerce-1.0/src/components/Card.jsx
+++ e-commerce-1.0/src/components/Card.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 function Card({ producto }) {
   return (
     <div key={producto.id} className="producto-card">
-      <h3 style={{ color: "black" }}>{producto.name}</h3>
-      <img
+      <h2 style={{ color: "black" }}>{producto.name}</h2>
+      <Link to={"/productos/" + producto.id}><img
         className="producto-image"
         src={producto.image}
         alt={producto.name}
-      />
+      /></Link>
       <p style={{ color: "black" }}>Precio: ${producto.price}</p>
       <Link to={"/productos/" + producto.id}>
         <button style={{ color: "black" }} className="producto-button">
